350de3c4c49af844d311f5d45bddc4091263168c
s <w@MacBookPro.attlocal.net>
2025-05-07 18:19:08 -0500

    Navigate Methods

---
+++ src/test/java/Tests/Day02_WebDriver/L05_DriverNavigateMethods.java
@@ -0,0 +1,29 @@
+package Tests.Day02_WebDriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class L05_DriverNavigateMethods {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+
+        // Go to the Test Otomasyonu homepage
+        driver.get("https://www.testotomasyonu.com");
+        Thread.sleep(1000);
+
+        // Go to the Wise Quarter homepage
+        driver.navigate().to("https://www.wisequarter.com");
+        Thread.sleep(1000);
+
+        // Go back to the Test Otomasyonu homepage
+        driver.navigate().back();
+        Thread.sleep(1000);
+
+        // Navigate forward to the Wise Quarter homepage again
+        driver.navigate().forward();
+        Thread.sleep(1000);
+
+        driver.quit();
+    }
+}
