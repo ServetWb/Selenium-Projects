de0ff5f43bb9b5e9a395cbaccf212795bae1d9a2
s <w@MacBookPro.attlocal.net>
2025-05-07 18:16:21 -0500

    Different Drivers

---
+++ src/test/java/Tests/Day02_WebDriver/L04_UsingDifferentDriver.java
@@ -0,0 +1,25 @@
+package Tests.Day02_WebDriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class L04_UsingDifferentDriver {
+
+
+    // If the company we are working for
+    // requests us to use a specific WebDriver instead of Selenium WebDriver,
+    // we must add that WebDriver to our project.
+
+    // WebDriver driver = new ChromeDriver();
+/*
+        WebDriverManager.chromedriver().
+
+    void setup();
+    WebDriver driver = new ChromeDriver();
+        driver.get("https://www.testotomasyonu.com");
+
+        Thread.sleep(3000);
+        driver.quit();
+
+ */
+}
