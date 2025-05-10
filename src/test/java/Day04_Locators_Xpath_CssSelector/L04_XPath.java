5dd5f3c3c9b130976944b7bc5dbcdfc18cdf7681
s <w@MacBookPro.ht.home>
2025-05-09 22:04:58 -0500

    Xpath information

---
+++ src/test/java/Day04_Locators_Xpath_CssSelector/L04_XPath.java
@@ -0,0 +1,27 @@
+package Day04_Locators_Xpath_CssSelector;
+
+public class L04_XPath {
+    public static void main(String[] args) {
+
+        /*
+    So far, we have learned 6 locators:
+    1- By.id()
+    2- By.name()
+    3- By.className()
+    4- By.tagName()
+    5- By.linkText()
+    6- By.partialLinkText()
+
+    These locators are specialized in their respective areas.
+    However, if an HTML element does not have an 'id' attribute, then By.id() cannot be used.
+    Similarly, if the HTML element is not a link, By.linkText() or By.partialLinkText() cannot be used.
+
+    What we need is a powerful LOCATOR that can identify *any* kind of element.
+
+    XPath and CSS Selector are designed to locate any type of web element,
+    making them more flexible and powerful than the basic locators.
+
+         */
+    }
+}
+
